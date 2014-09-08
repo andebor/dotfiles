788c9bd5653700dbf5b5008c8a1db48b97961e55
andebor <post@andersborud.com>
2014-09-08 14:30:55 +0200

    added bash_profile

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc scripts"    # list of files/folders to symlink in homedir
+files="bashrc vimrc scripts bash_profile"    # list of files/folders to symlink in homedir
 
 ##########
 
