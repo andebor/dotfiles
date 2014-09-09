19f64e248a66dfbeff408bd67da0bd86781b1d18
andebor <post@andersborud.com>
2014-09-09 13:26:44 +0200

    added script to check repo for bashrc updates

---
+++ scripts/checkGit.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+######################
+# checkgit.sh
+# This script checks remote git repo to see if local repo is up to date and
+# download any changes.
+######################
+
+dir=~/dotfiles     # local git repo
+
+if ! git --git-dir="$dir" diff --quiet
+then
+	git --git-dir="$dir" pull origin master
+	echo "Lastest bashrc was downloaded!"
+else
+	echo "bashrc is up to date!"
+fi
