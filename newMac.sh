ed9b7b55ce7b8b54303afab10b52004915ab076a
Anders Borud <post@andersborud.com>
2015-04-07 16:26:31 +0200

    Added install script for mac installation and added some matching changes to zshrc.

---
+++ newMac.sh
@@ -0,0 +1,246 @@
+#! /bin/bash
+
+# Script to install my everyday terminal dependencies and shell settings on a new mac
+
+#TODO:
+# Find out how to install xcode in script
+# Debian version
+
+##########  DEFAULT INSTALL CONFIG  #############
+
+#brew software to be installed
+BREWS="git coreutils wget z caskroom/cask/brew-cask node"
+
+# pip packages to be installed
+PIPS="virtualenv setuptools autoenv"
+
+#cask software to be installed
+CASKS="google-chrome google-drive dropbox alfred spotify sublime-text thunderbird slack iterm2 divvy caffeine deluge vlc" 
+
+#files to symlink into homedir
+FILES="zshrc vimrc scripts"
+
+##################################################
+
+MAIN_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#check if user provided own config
+if [[ $1 == -c ]]; then
+    CONFIG_FILE=$2
+fi
+
+if [[ -f $CONFIG_FILE ]]; then
+    source $CONFIG_FILE
+fi
+
+# Colors
+red="\033[0;31m"
+yellow="\033[0;33m"
+green="\033[0;32m"
+purple="\033[0;35m"
+NC="\033[0m"
+
+echo -e "${green}###################################"
+echo -e "#  Installation script to set up  #"
+echo -e "#   a fresh OS X installation     #"
+echo -e "###################################"
+echo ""
+echo -e "This installation will install the following software:"
+echo -e "------------------------------------------------------"
+echo -e "Terminal apps/Homebrew/pip"
+for brew in $BREWS; do
+    echo -e "-- "$brew
+done
+for pip in $PIPS; do
+    echo -e "-- "$pip
+done
+echo -e ""
+echo -e "Desktop applications (with cask)"
+for cask in $CASKS; do
+    echo -e "-- "$cask
+done
+echo -e "------------------------------------------------------"
+echo -e "To customize which applications to install, run the script with -c followed by the path to your config file."
+echo -e "See the example file, for more information."
+
+echo ""
+echo -e "${red}Make sure you are connected to the internet before you procede!${NC}"
+echo ""
+#check if user want to continue
+read -p "Do you want to procede with the installation? (y/n)" response </dev/tty
+if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "Exiting installation..."
+    exit 0
+fi
+
+# Get sudo from user
+echo -e "${yellow}You might need to input your sudo password${NC}"
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+#install xcode - https://sector7g.be/posts/installing-xcode-command-line-tools-through-terminal-without-any-user-interaction
+# echo "Checking if xcode is installed.."
+# if ! which make > /dev/null; then
+#   echo -e "${yellow}Xcode not installed. Installing now...${NC}"
+#   echo "Creating file to override xcode agreement prompt."
+#   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+#   echo "Running softwareupdate to install xcode."
+#   sudo softwareupdate -i -a
+# else
+#   echo -e "${green}Xcode is already installed.${NC}"  
+
+
+# Change hostname
+read -p "Would you like to set your computer name (as done via System Preferences >> Sharing)?  (y/n)" response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  echo "What would you like it to be?"
+  read COMPUTER_NAME
+  sudo scutil --set ComputerName $COMPUTER_NAME
+  sudo scutil --set HostName $COMPUTER_NAME
+  sudo scutil --set LocalHostName $COMPUTER_NAME
+  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+  echo -e "${green}Computer name was changed to $COMPUTER_NAME${NC}"
+fi
+
+#Install homebrew
+echo "Checking if Homebrew is installed.."
+if ! which brew > /dev/null; then
+    echo -e "${yellow}Homebrew not installed. Installing now..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo -e "Running brew doctor...${NC}"
+    brew doctor
+    echo -e "${green}OK${NC}"
+else
+    echo -e "${green}Homebrew is already installed.${NC}"
+fi
+
+#Install homebrew packages
+echo "Verifying that all brew packages are installed.."
+sudo brew install ${BREWS}
+echo -e "${green}OK${NC}"
+
+#Install desktop software
+echo "Verifying that all cask packages are installed.."
+sudo brew cask install ${CASKS}
+echo -e "${green}OK${NC}"
+
+#Install zsh and oh-my-zsh
+echo -e "${yellow}Checking if oh-my-zsh is installed.. ${NC}"
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo -e "${yellow}Oh-My-Zsh not installed. Installing now...${NC}"
+    curl -L http://install.ohmyz.sh | sh
+    echo -e "${green}OK${NC}"
+else
+    echo -e "${yellow}Oh-My-Zsh is already installed.${NC}"
+fi
+
+# Syntax highlighting
+echo -e "${yellow}Checking if syntax highlighting is installed.."
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    echo -e "Syntax highlighting not installed. Installing now...${NC}"
+    cd ~/.oh-my-zsh/custom/plugins && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git 
+    cd $MAIN_DIR
+    echo -e "${green}OK${NC}"
+
+else
+    echo -e "${green}oh-my-zsh already installed${NC}"
+fi
+
+#Set colortheme in iterm2
+read -p "Do you want to change the color theme in iterm2 to Solarize? (y/n)" response </dev/tty
+if [[  $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist.old
+    cp $MAIN_DIR/setup/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+    plutil -convert binary1 ~/Library/Preferences/com.googlecode.iterm2.plist
+    defaults read com.googlecode.iterm2
+    echo -e "${green}iTerm2 is now solarized!${NC}"
+fi
+
+#install pip
+echo "Checking if Pip is installed.."
+if ! which pip > /dev/null; then
+    echo -e "${yellow}Pip not installed. Installing now...${NC}"
+    sudo easy_install pip
+    echo -e "${green}OK${NC}"
+else
+    echo -e "${green}Pip is already installed.${NC}"
+fi
+
+#install pip packages
+echo "Verifying that all pip packages are installed."
+sudo pip install ${PIPS}
+echo -e "${green}OK${NC}"
+
+# Do OS X modifications
+echo "Doing OS X modifications"
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Expanding the save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Use column view in all Finder windows by default
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups (This can take up a ton of SSD space on <128GB SSDs)
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+# Disable the all too sensitive backswipe in chrome
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+
+
+# Make symlinks for zshrc, dircolors
+current_time=$(date "+%Y-%m-%d.%H:%M:%S")
+
+read -p "Do you want to put symlinks for your dotfiles in the your home dir? (y/n)" response </dev/tty
+if [[  $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    for file in $FILES; do
+        mv ~/.$file ~/dotfiles_${current_time}/ # TODO: should add timestamp to foldername
+        echo "Creating symlink to $file in home directory."
+        ln -s $MAIN_DIR/$file ~/.$file
+    done
+    echo -e "${green}Symlinks created!${NC}"
+fi
+
+# Put host_variables file in oh-my-zsh dir if not present
+if [[ ! -f ~/.host_variables ]]; then
+    cp $MAIN_DIR/setup/host_variables ~/.host_variables
+fi
+
+#Ask user for reboot
+echo "The computer needs to be rebooted before all the changes will take effect.${yellow}"
+read -p "Do you want to reboot now? (y/n)" response </dev/tty
+if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo -e "${NC}Rebooting..."
+    sudo shutdown -r now
+else
+    echo -e "${green}Installation is finished. Please restart as soon as possible.${NC}"
+fi
---
+++ zshrc
@@ -0,0 +1,103 @@
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="pygmalion"
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment the following line to change how often to auto-update (in days).
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(brew django git npm pip python screen sublime sudo virtualenvwrapper jump virtualenv-prompt)
+
+# User configuration
+
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/texbin"
+# export MANPATH="/usr/local/man:$MANPATH"
+
+source $ZSH/oh-my-zsh.sh
+
+# You may need to manually set your language environment
+# export LC_ALL=en_DK.utf8 && export LANG=en_DK.utf8
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='vim'
+fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/dsa_id"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# source z
+. `brew --prefix`/etc/profile.d/z.sh
+
+
+# dircolors
+eval `gdircolors ~/.scripts/.dir_colors`
+alias ls='gls --color'
+zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
+autoload -Uz compinit
+compinit
+
+# source host_variables and tell user if the file is missing.
+if [[ ! -f ~/.host_variables ]]; then
+    echo -e "\033[0;31mMissing host_variables file! Please provide this file before using aliases!\033[0m"
+else
+    source ~/.host_variables
+fi
+
+# source scripts in dotfiles
+for file in ~/.scripts/source*; do
+    source $file
+done
