87381b65e308561893fc04c54a04a3585b99a0da
Anders Borud <post@andersborud.com>
2015-04-11 16:53:45 +0200

    Changed virtualenv naming convention.

--- scripts/source/virtualenv.zsh
+++ scripts/source/virtualenv.zsh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
 function makeenv() {
+    # get directory name only
+    DIRNAME=${PWD##*/}
     # Get working directory
     WORKINGDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
     
     #Check shell for to determine read syntax
     if [[ "$SHELL" == "/bin/zsh" ]]; then
-        read -q "response?Do you want to make this directory($WORKINGDIR) a virtualenv? (y/n)"
+        read -q "response?Do you want to create a virtualenv inside this directory($WORKINGDIR)? (y/n)"
     else
-        read -p "Do you want to make this directory($WORKINGDIR) a virtualenv? (y/n)" -n 1 response
+        read -p "Do you want to create a virtualenv inside this directory($WORKINGDIR)? (y/n)" -n 1 response
     fi
 
     echo ""
 
     if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
         echo "Aborting..."
-        echo $DIRNAME
         return
     fi
     #make enviroment
-    virtualenv $WORKINGDIR
+    # ENVNAME="env_$DIRNAME"
+    # virtualenv $ENVNAME
+    virtualenv "env_$DIRNAME"
 
     # make .env file for autoenv
-    echo "source $WORKINGDIR/bin/activate" > .env
+    echo "Creating .env file for autoenv.."
+    echo "source $WORKINGDIR/env_$DIRNAME/bin/activate" > .env
+
+    #activate new environment
+    echo "Activating new environment.."
+    source $WORKINGDIR/env_$DIRNAME/bin/activate
 }
