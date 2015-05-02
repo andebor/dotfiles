4e826fa2291d2c0972c6217cd773263a4374305d
Anders Borud <post@andersborud.com>
2015-05-02 23:07:16 +0200

    Made the script get hosts from the .host_variables file.

--- scripts/deploy_ssh_keys.sh
+++ scripts/deploy_ssh_keys.sh
@@ -2,12 +2,15 @@
 
 # Tiny script do deploy ssh-keys to multiple hosts. Loops through an array of environment variables declared in zshrc/bashrc. Creates key if not found. Gives user multiple password prompts.
 
-HOSTS="$ROCKY $RAMBO $MACGYVER $STUD"
+HOSTS=$(sed -n "/^export/p" $HOME/.host_variables | awk -F '[ =]' '{print $2}' | sed -r 's/[^ ]+/$&/g')
+# This is based on me having a file that exports all the hosts as environmental variables in home dir.
+# If you want a simpler solution, you can just do:
+# HOSTS="example1.com example2.com example3.com"
 EMAIL="post@$RAMBO"
 
-echo "This script will deploy your ssh-key to the following hosts:"
+echo "This script will deploy your ssh-key to the following hosts with username $USER:"
 for host in $HOSTS; do
-	echo "- $host" 
+	eval echo "- $host" 
 done
 
 read -p "Do you want to procede? (y/n)" -n 1 response </dev/tty
@@ -22,5 +25,5 @@ if [[ ! -f $HOME/.ssh/id_rsa.pub ]]; then
 fi
 
 for host in $HOSTS; do
-	cat ~/.ssh/id_rsa.pub | ssh andebor@$host " cat >> ~/.ssh/authorized_keys"
+	cat ~/.ssh/id_rsa.pub | eval ssh $USER@$host " cat >> ~/.ssh/authorized_keys"
 done
