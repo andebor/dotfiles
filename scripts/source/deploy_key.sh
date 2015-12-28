bcd1f09caeef45ff53725643fb5d88f692b3407b
Anders Borud <post@andersborud.com>
2015-12-28 19:22:07 +0100

    Add more feedback from rsa deploy script

--- scripts/source/deploy_key.sh
+++ scripts/source/deploy_key.sh
@@ -3,13 +3,16 @@ function deploy_key () {
 	# check for argument
 	if [ $# -eq 0 ]; then
     	echo "No host supplied"
-    	exit 0
+    	return 1
 	fi
 	#check if pub key exist. create if not
 	if [[ ! -f $HOME/.ssh/id_rsa.pub ]]; then
 		ssh-keygen -t rsa -C post@andersborud.com
 	fi
-	cat ~/.ssh/id_rsa.pub | ssh $USER@$1 " mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
-	echo "Added rsa public key to authorized keys on $1."
+	if cat ~/.ssh/id_rsa.pub | ssh $USER@$1 " mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"; then
+			echo "Added rsa public key to authorized keys on $1."
+	else
+			echo "Something went wrong. See error message above."
+	fi
 
 }
