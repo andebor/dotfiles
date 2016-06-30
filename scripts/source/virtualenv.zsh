b2c58345dbcb1f14726cc6b49d73c0a566a4520c
Anders Borud <post@andersborud.com>
2016-06-30 08:30:16 +0200

    Fix error message from conditional expressions

--- scripts/source/virtualenv.zsh
+++ scripts/source/virtualenv.zsh
@@ -8,13 +8,13 @@ function makeenv() {
     
     #Check shell for to determine read syntax
     if [[ "$SHELL" == "/bin/zsh" ]]; then
-      if [ $# -eq 1 ];then
+      if [[ $# -eq 1 ]];then
         read -q "response?Do you want to create a python2.7 virtualenv inside this directory($WORKINGDIR)? (y/n)"
       else
         read -q "response?Do you want to create a python3 virtualenv inside this directory($WORKINGDIR)? (y/n)"
       fi
     else
-      if [ $# -eq 1 ];then
+      if [[ $# -eq 1 ]];then
         read -p "Do you want to create a python2.7 virtualenv inside this directory($WORKINGDIR)? (y/n)" -n 1 response
       else
         read -p "Do you want to create a python3 virtualenv inside this directory($WORKINGDIR)? (y/n)" -n 1 response
@@ -30,9 +30,9 @@ function makeenv() {
     #make enviroment
     # ENVNAME="env_$DIRNAME"
     # virtualenv $ENVNAME
-    if [ $1 -eq 2 ];then
+    if [[ $1 -eq 2 ]];then
       virtualenv -p python "env_$DIRNAME"
-    elif [ $1 -eq 3 ];then
+    elif [[ $1 -eq 3 ]];then
       virtualenv -p python3 "env_$DIRNAME"
     else
       virtualenv -p python3 "env_$DIRNAME"
